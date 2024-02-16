b001d94e84c6cefedd57f2c945ecb1354e85d013
Classican67 <jeremy.maitre.1@ulaval.ca>
2024-02-16 18:52:39 -0500

    Insertion de la variable global afin de refleter la clinique dans le breadcrumb

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+ClinicGlobal = {}
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    index = 1
+    return render_template("index.html", index=index)
+
+
+@app.route("/patients")
+def patients():
+    index = 2
+    return render_template("patientPage.html", index=index, ClinicGlobal=ClinicGlobal["clinique"])
+
+
+@app.route("/update_clinic", methods=["GET"])
+def update_clinic():
+    selected_option = request.args.get("selected_option")
+    ClinicGlobal["clinique"] = selected_option
+    response_data = {"message": "Option sélectionnée : " + selected_option}
+    return jsonify(response_data)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
