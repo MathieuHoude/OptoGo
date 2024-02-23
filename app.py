b5b23ed1b2f8f2895edd6adfc991013bedaddfaa
Classican67 <jeremy.maitre.1@ulaval.ca>
2024-02-23 12:46:53 -0500

    - Ajout commentaire - Ajout d'une variable Globale pour les infos de l'Opto ( 70% fonctionnelle) - Reglage de bug pour le chargement de la variable de la clinique avec ajout de Snack-bar

--- app.py
+++ app.py
@@ -2,14 +2,26 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-ClinicGlobal = {}
+ClinicGlobal = {"clinique": ""}
+
+OptoInfoGLobal = {
+    "PracticeNumber": '123456',
+    "Adresse": '2250 rue Sicard',
+    "Phone": '4508480147'
+}
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     index = 1
-    return render_template("index.html", index=index)
+    return render_template(
+        "index.html",
+        index=index,
+        PracticeNumber=OptoInfoGLobal["PracticeNumber"],
+        Adresse=OptoInfoGLobal["Adresse"],
+        Phone=OptoInfoGLobal["Phone"]
+    )
 
 
 @app.route("/patients")
@@ -24,6 +36,19 @@ def update_clinic():
     ClinicGlobal["clinique"] = selected_option
     response_data = {"message": "Option sélectionnée : " + selected_option}
     return jsonify(response_data)
+
+
+@app.route("/update_opto", methods=["GET"])
+def update_opto():
+    new_practice_number = request.args.get("practice_number")
+    new_adresse = request.args.get("adresse")
+    new_phone_number = request.args.get("phone_number")
+    OptoInfoGLobal["PracticeNumber"] = new_practice_number
+    OptoInfoGLobal["Adresse"] = new_adresse
+    OptoInfoGLobal["Phone"] = new_phone_number
+    response_data = {"message": "Informations de l'optométriste mises à jour avec succès"}
+    return jsonify(response_data)
+
 
 
 if __name__ == '__main__':
