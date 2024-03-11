e810abe2d624bd03642a971e9685263d03d5ef80
MathieuHoude <mathieu.houde@codeboxx.biz>
2024-03-11 09:28:08 -0400

    liste patients db

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+import os
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
+# Load environment variables from .env file
+load_dotenv()
+
 ClinicGlobal = {}
+
+def get_db_connection():
+    # Replace these with your actual database connection details
+    # MySQL configuration
+    mysql_config = {
+        'host': 'localhost',
+        'user': os.getenv('USERNAME'),
+        'password': os.getenv('PASSWORD'),
+        'database': os.getenv('DBNAME')
+    }
+
+    # Connect to the MySQL server
+    try:
+        connection = mysql.connector.connect(**mysql_config)
+
+        if connection.is_connected():
+            return connection
+
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
 
 
 @app.route("/")
@@ -15,7 +42,12 @@ def index():
 @app.route("/patients")
 def patients():
     index = 2
-    return render_template("patientPage.html", index=index, ClinicGlobal=ClinicGlobal["clinique"])
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM patients LIMIT 100')
+    patients = cursor.fetchall() 
+    conn.close()
+    return render_template("patientPage.html", index=index, ClinicGlobal=ClinicGlobal["clinique"], patients=patients)
 
 
 @app.route("/update_clinic", methods=["GET"])
