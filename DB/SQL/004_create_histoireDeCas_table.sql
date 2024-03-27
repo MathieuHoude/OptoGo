3841524625d9cd1a126c72e2910b5d4ce2f971a7
justingiguere <justin.giguere29@gmail.com>
2024-03-27 11:52:37 -0400

    Modification des attributs en JSON

--- DB/SQL/004_create_histoireDeCas_table.sql
+++ DB/SQL/004_create_histoireDeCas_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS histoireDeCas (
     ID INT AUTO_INCREMENT PRIMARY KEY,
-    conditions VARCHAR(50) NOT NULL,
-    allergies VARCHAR(50) NOT NULL,
-    medications VARCHAR(50) NOT NULL,
-    trouble_vision VARCHAR(50) NOT NULL,
-    antecedants_familiaux VARCHAR(50) NOT NULL,
-    antecedants_oculaires VARCHAR(50) NOT NULL,
+    conditions JSON NOT NULL,
+    allergies JSON NOT NULL,
+    medications JSON NOT NULL,
+    trouble_vision JSON NOT NULL,
+    antecedants_familiaux JSON NOT NULL,
+    antecedants_oculaires JSON NOT NULL,
     examens_ID INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
