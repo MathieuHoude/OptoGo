cbe9ad28a789ad550fbd2e306a27d65abd3497ae
justingiguere <justin.giguere29@gmail.com>
2024-03-27 14:14:58 -0400

    Enlever les commentaires

--- DB/SQL/006_create_examens.sql
+++ DB/SQL/006_create_examens.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS examens (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     RX_objective JSON NOT NULL,
     RX_subjective JSON NOT NULL,
-    adjustment JSON NOT NULL, --Port de verre de contact
-    lens_type JSON NOT NULL, --Type de lunette porté
+    adjustment JSON NOT NULL ,
+    lens_type JSON NOT NULL,
     old_RX JSON NOT NULL,
     patient_ID INTEGER NOT NULL,
     optometriste_ID INTEGER NOT NULL REFERENCES optometristes(ID) ON DELETE NO ACTION,
