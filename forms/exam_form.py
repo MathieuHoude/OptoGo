e7fa4ccbf0144a3e8bd08777dc9aeafa4a9ceb3b
MathieuHoude <mathieu.houde@codeboxx.biz>
2024-04-05 09:56:27 -0400

    saving exams in progress

--- forms/exam_form.py
+++ forms/exam_form.py
@@ -1,61 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, FloatField, IntegerField, SelectField
+from wtforms import BooleanField, FloatField, IntegerField, SelectField, StringField
 from wtforms.validators import NumberRange, ValidationError
 
 def validate_rx_add(form, field):
-    value = field.data
+    value = float(field.data)
     if value % 0.25 != 0:
         raise ValidationError('Value must be in steps of 0.25')
 
 class ExamForm(FlaskForm):
-    conditions_diabetes = BooleanField('Diabète')
-    conditions_hypertension = BooleanField('Hypertension artérielle')
-    conditions_heart_condition = BooleanField('Troubles cardiaques')
-    conditions_asthma = BooleanField('Asthme')
-    conditions_cholesterol = BooleanField('Taux de cholesterol élevé')
-    allergies_topical_antibiotics = BooleanField('Antibiotiques topiques')
-    allergies_topical_corticosteroids = BooleanField('Corticostéroïdes topiques')
-    allergies_decongestant_eye_drops = BooleanField('Collyres de décongestion')
-    allergies_anesthetics = BooleanField('Anesthésiques')
-    allergies_preservatives = BooleanField('Conservateurs dans les collyres')
-    medications_amiodarone = BooleanField('Amiodarone')
-    medications_digoxin = BooleanField('Digoxine')
-    medications_isotretinoin = BooleanField('Isotrétinoïne')
-    medications_chloroquine = BooleanField('Chloroquine')
-    medications_methylprednisolone = BooleanField('Méthylprednisolone')
-    trouble_vision_glaucoma = BooleanField('Glaucome')
-    trouble_vision_cataract = BooleanField('Cataracte')
-    trouble_vision_strabismus = BooleanField('Strabisme')
-    trouble_vision_double_vision = BooleanField('Vision double')
-    trouble_vision_flash = BooleanField('Perception de flash')
-    trouble_vision_floaters = BooleanField('Mouches volantes')
-    trouble_vision_macular_degeneration = BooleanField('Dégénérescence maculaire')
-    antecedants_familiaux_retinal_detachment = BooleanField('Décollement de la rétine')
-    antecedants_familiaux_macular_degeneration = BooleanField('Dégénérescence maculaire')
-    antecedants_familiaux_glaucoma = BooleanField('Glaucome')
-    antecedants_oculaires_surgery = BooleanField('Chirurgie aux yeux')
-    antecedants_oculaires_trauma = BooleanField('Traumatisme aux yeux')
-    antecedants_oculaires_retinal_detachment = BooleanField('Décollement de la rétine')
+    ID = StringField('ID')
+    patient_ID = IntegerField('ID patient')
+    optometriste_ID = IntegerField('ID optometriste')
+    histoireDeCas_ID = IntegerField('ID histoire de cas')
+    choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
+    old_RX_Sphere_LE = SelectField('Sphère', choices=choices, validators=[validate_rx_add])
+    old_RX_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
+    choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
+    old_RX_Axis_LE = SelectField('Axe', choices=choices)
+    choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
+    old_RX_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add])
+    choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
+    old_RX_Acuity_LE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Sphere_LE = SelectField('Sphère', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    old_RX_Ast_LE = SelectField('Ast', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
+    old_RX_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    old_RX_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    old_RX_Axis_LE = SelectField('Axe', choices=choices, validators=[NumberRange(min=0, max=180)])
+    old_RX_Axis_RE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Add_LE = SelectField('Add', choices=choices, validators=[NumberRange(min=0, max=5), validate_rx_add])
+    old_RX_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
-    old_RX_Acuity_LE = SelectField('Acuité', choices=choices, validators=[NumberRange(min=0.33, max=1)])
-
-    choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Sphere_RE = SelectField('Sphere', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    old_RX_Ast_RE = SelectField('Ast', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    old_RX_Axis_RE = SelectField('Axe', choices=choices, validators=[NumberRange(min=0, max=180)])
-    choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Add_RE = SelectField('Add', choices=choices, validators=[NumberRange(min=0, max=5), validate_rx_add])
-    choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
-    old_RX_Acuity_RE = SelectField('Acuité', choices=choices, validators=[NumberRange(min=0.33, max=1)])
+    old_RX_Acuity_RE = SelectField('Acuité', choices=choices)
 
     lens_type_single_vision_lenses = BooleanField('Verres simple vision')
     lens_type_progressive_lenses = BooleanField('Verres progressifs')
@@ -67,41 +42,43 @@ class ExamForm(FlaskForm):
     contact_lens_type_mono_vision_contact_lenses = BooleanField('VC mono-vision')
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    RX_objective_Ast_LE = SelectField('Ast', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
+    RX_objective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_objective_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_objective_Axis_LE = SelectField('Axe', choices=choices, validators=[NumberRange(min=0, max=180)])
+    RX_objective_Axis_LE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Add_LE = SelectField('Add', choices=choices, validators=[NumberRange(min=0, max=5), validate_rx_add])
+    RX_objective_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
-    RX_objective_Acuity_LE = SelectField('Acuité', choices=choices, validators=[NumberRange(min=0.33, max=1)])
+    RX_objective_Acuity_LE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    RX_objective_Ast_RE = SelectField('Ast', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
+    RX_objective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_objective_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_objective_Axis_RE = SelectField('Axe', choices=choices, validators=[NumberRange(min=0, max=180)])
+    RX_objective_Axis_RE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Add_RE = SelectField('Add', choices=choices, validators=[NumberRange(min=0, max=5), validate_rx_add])
+    RX_objective_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
-    RX_objective_Acuity_RE = SelectField('Acuité', choices=choices, validators=[NumberRange(min=0.33, max=1)])
+    RX_objective_Acuity_RE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    RX_subjective_Ast_LE = SelectField('Ast', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
+    RX_subjective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_subjective_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_subjective_Axis_LE = SelectField('Axe', choices=choices, validators=[NumberRange(min=0, max=180)])
+    RX_subjective_Axis_LE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Add_LE = SelectField('Add', choices=choices, validators=[NumberRange(min=0, max=5), validate_rx_add])
+    RX_subjective_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
-    RX_subjective_Acuity_LE = SelectField('Acuité', choices=choices, validators=[NumberRange(min=0.33, max=1)])
+    RX_subjective_Acuity_LE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
-    RX_subjective_Ast_RE = SelectField('Ast', choices=choices, validators=[NumberRange(min=-20.0, max=20.0), validate_rx_add])
+    RX_subjective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_subjective_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_subjective_Axis_RE = SelectField('Axe', choices=choices, validators=[NumberRange(min=0, max=180)])
+    RX_subjective_Axis_RE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Add_RE = SelectField('Add', choices=choices, validators=[NumberRange(min=0, max=5), validate_rx_add])
+    RX_subjective_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
-    RX_subjective_Acuity_RE = SelectField('Acuité', choices=choices, validators=[NumberRange(min=0.33, max=1)])
+    RX_subjective_Acuity_RE = SelectField('Acuité', choices=choices)
+
+    periode_validite = IntegerField('Période de validité')
--- routes/examens_routes.py
+++ routes/examens_routes.py
@@ -3,6 +3,7 @@ from mysql.connector import Error
 import json
 
 from forms.exam_form import ExamForm
+from forms.histoireDeCas_form import HistoireDeCasForm
 from DB.utils import get_db_connection
 from utils import update_session
 
@@ -46,17 +47,24 @@ def details(examen_id):
         cursor = conn.cursor(dictionary=True)
         update_session(cursor, "clinique", f"SELECT * FROM cliniques WHERE ID = {clinique_id}")
         update_session(cursor, "patient", f"SELECT * FROM patients WHERE ID = {patient_id}")
-        update_session(cursor, "examen", f"SELECT * FROM examens e LEFT JOIN histoireDeCas h ON e.ID = h.examen_ID WHERE e.ID = {examen_id}" )
+        update_session(cursor, "examen", f"SELECT * FROM examens e WHERE e.ID = {examen_id}")
+        cursor.execute(f"SELECT * FROM histoireDeCas h WHERE h.ID = {session['examen']['histoireDeCas_ID']}")
+        hdc = cursor.fetchone()
         conn.close()
-        session["examen"] = parse_exam_json_objects(session["examen"])
-        form = ExamForm(data=session["examen"])
+        session["examen"] = parse_json_objects(session["examen"])
+        hdc = parse_json_objects(hdc)
+        exam_form = ExamForm(data=session["examen"])
+        hdc_form = HistoireDeCasForm(data=hdc)
+        confirmation_message = session.pop('confirmation_message', None)
         return render_template("examDetailsPage.html",
                             index=index,
                             clinique=session["clinique"],
                             optometriste=session["user"],
                             patient=session["patient"],
                             examen=session["examen"],
-                            form=form
+                            confirmation_message=confirmation_message,
+                            exam_form=exam_form,
+                            hdc_form=hdc_form
                             )
     except Error as e:
         print(f"Error connecting to MySQL: {e}")
@@ -64,13 +72,12 @@ def details(examen_id):
 
 
 # route pour la page d'un nouvel examen
-@examens_bp.route("/new")
+@examens_bp.route("/new", methods=['GET', 'POST'])
 def new():
+    form = ExamForm()
     try:
-        form = ExamForm()
-        clinique_id = request.args.get('clinique_id')
-        patient_id = request.args.get('patient_id')
         index = 5
+        confirmation_message = session.pop('confirmation_message', None)
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True)
         cursor.execute(f'SELECT * FROM cliniques WHERE ID = {clinique_id}')
@@ -83,6 +90,7 @@ def new():
                             clinique=ClinicGlobal,
                             optometriste=session["user"],
                             patient=PatientSelect,
+                            confirmation_message=confirmation_message,
                             form=form
                             )
     except Error as e:
@@ -106,7 +114,7 @@ def prescription(examen_id):
 
         cursor.close()
         conn.close()
-        session["examen"] = parse_exam_json_objects(session["examen"])
+        session["examen"] = parse_json_objects(session["examen"])
         form = ExamForm(data=session["examen"])
         return render_template("prescription/prescriptionPage.html",
                             index=index,
@@ -120,9 +128,171 @@ def prescription(examen_id):
     except Error as e:
         print(f"Error connecting to MySQL: {e}")
 
+# route pour sauvegarder l'histoire de cas
+@examens_bp.route("/submit_histoireDeCas", methods=['POST'])
+def submit_histoireDeCas():
+    form = HistoireDeCasForm()
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
 
+    if form.validate_on_submit():
+        histoireDeCas_data = {
+            'conditions': {
+                "asthma": form.conditions_asthma.data, 
+                "diabetes": form.conditions_diabetes.data,
+                "cholesterol": form.conditions_cholesterol.data, 
+                "hypertension": form.conditions_hypertension.data, 
+                "heart_condition": form.conditions_heart_condition.data
+            },
+            'allergies': {
+                "anesthetics": form.allergies_anesthetics.data, 
+                "preservatives": form.allergies_preservatives.data, 
+                "topical_antibiotics": form.allergies_topical_antibiotics.data, 
+                "decongestant_eye_drops": form.allergies_decongestant_eye_drops.data, 
+                "topical_corticosteroids": form.allergies_topical_corticosteroids.data
+            },
+            'medications': {
+                "digoxin": form.medications_digoxin.data, 
+                "amiodarone": form.medications_amiodarone.data, 
+                "chloroquine": form.medications_chloroquine.data, 
+                "isotretinoin": form.medications_isotretinoin.data, 
+                "methylprednisolone": form.medications_methylprednisolone.data
+            },
+            'trouble_vision': {
+                "flash": form.trouble_vision_flash.data, 
+                "cataract": form.trouble_vision_cataract.data, 
+                "floaters": form.trouble_vision_floaters.data, 
+                "glaucoma": form.trouble_vision_glaucoma.data, 
+                "strabismus": form.trouble_vision_strabismus.data, 
+                "double_vision": form.trouble_vision_double_vision.data, 
+                "macular_degeneration": form.trouble_vision_macular_degeneration.data
+            },
+            'antecedants_familiaux': {
+                "glaucoma": form.antecedants_familiaux_glaucoma.data, 
+                "retinal_detachment": form.antecedants_familiaux_retinal_detachment.data, 
+                "macular_degeneration": form.antecedants_familiaux_macular_degeneration.data
+            },
+            'antecedants_oculaires': {
+                "trauma": form.antecedants_oculaires_trauma.data, 
+                "surgery": form.antecedants_oculaires_surgery.data, 
+                "retinal_detachment": form.antecedants_oculaires_retinal_detachment.data
+            },
+            'notes': form.notes.data
+        }
+        # Convert each sub-dictionary to JSON strings
+        for key, value in histoireDeCas_data.items():
+            if type(value) == dict:
+                try:
+                    histoireDeCas_data[key] = json.dumps(value)
+                except (json.JSONDecodeError, TypeError):
+                    pass
+        if form.ID.data: #Existing hdc
+            # cursor.execute(f'SELECT * FROM histoireDeCas WHERE ID = {form.ID.data}')
+            # hdc = cursor.fetchone()
+            # modified_fields = {}
+            # for field in form: 
+            #     if field.name != 'csrf_token' and field.data != hdc[field.name]:
+            #         modified_fields[field.name] = field.data
 
-def parse_exam_json_objects(dict):
+            # if modified_fields:
+            #     sql_update_query = "UPDATE histoireDeCas SET "
+            #     sql_update_query += ", ".join([f"{field} = %s" for field in modified_fields.keys()])
+            #     sql_update_query += " WHERE id = %s"
+                
+            #     # Create a tuple of parameter values in the same order as placeholders
+            sql_query = "UPDATE histoireDeCas SET conditions = %s, allergies = %s, medications = %s, trouble_vision = %s, antecedants_familiaux = %s, antecedants_oculaires = %s, notes = %s WHERE ID = %s"
+            params = tuple(histoireDeCas_data.values()) + (form.ID.data,)
+        else: #New hdc
+            sql_query = "INSERT INTO histoireDeCas (conditions, allergies, medications, trouble_vision, antecedants_familiaux, antecedants_oculaires, notes) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            params = tuple(histoireDeCas_data.values())
+        cursor.execute(sql_query, params)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        session['confirmation_message'] = {"title": "Histoire de cas sauvegardée", "text": f"L'histoire de cas de l'examen en cours a été sauvegardée avec succès."}
+    return redirect(url_for("examens.details",examen_id=f"{session['examen']['ID']}", clinique_id=f"{session['clinique']['ID']}", patient_id=f"{session['patient']['ID']}"))
+
+@examens_bp.route("/submit_examen", methods=['POST'])
+def submit_examen():
+    form = ExamForm()
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    # clinique_id = request.args.get('clinique_id')
+    patient_id = request.args.get('patient_id')
+    if request.method == 'POST' and form.validate_on_submit():
+        new_exam_data = {
+            'RX_objective': {
+                "Add_LE": form.RX_objective_Add_LE.data, 
+                "Add_RE": form.RX_objective_Add_RE.data, 
+                "Ast_LE": form.RX_objective_Ast_LE.data, 
+                "Ast_RE": form.RX_objective_Ast_RE.data, 
+                "Axis_LE": form.RX_objective_Axis_LE.data, 
+                "Axis_RE": form.RX_objective_Axis_RE.data, 
+                "Acuity_LE": form.RX_objective_Acuity_LE.data, 
+                "Acuity_RE": form.RX_objective_Acuity_RE.data, 
+                "Sphere_LE": form.RX_objective_Sphere_LE.data, 
+                "Sphere_RE": form.RX_objective_Sphere_RE.data
+            },
+            'RX_subjective': {
+                "Add_LE": form.RX_subjective_Add_LE.data, 
+                "Add_RE": form.RX_subjective_Add_RE.data, 
+                "Ast_LE": form.RX_subjective_Ast_LE.data, 
+                "Ast_RE": form.RX_subjective_Ast_RE.data, 
+                "Axis_LE": form.RX_subjective_Axis_LE.data, 
+                "Axis_RE": form.RX_subjective_Axis_RE.data, 
+                "Acuity_LE": form.RX_subjective_Acuity_LE.data, 
+                "Acuity_RE": form.RX_subjective_Acuity_RE.data, 
+                "Sphere_LE": form.RX_subjective_Sphere_LE.data, 
+                "Sphere_RE": form.RX_subjective_Sphere_RE.data
+            },
+            'contact_lens_type': {"multifocal_contact_lenses": form.contact_lens_type_multifocal_contact_lenses.data, 
+                                "mono_vision_contact_lenses": form.contact_lens_type_mono_vision_contact_lenses.data, 
+                                "single_vision_contact_lenses": form.contact_lens_type_single_vision_contact_lenses.data},
+            'lens_type': {"office_lenses": form.lens_type_office_lenses.data, 
+                        "bifocal_lenses": form.lens_type_bifocal_lenses.data, 
+                        "progressive_lenses": form.lens_type_progressive_lenses.data, 
+                        "single_vision_lenses": form.lens_type_single_vision_lenses.data},
+            'periode_validite': form.periode_validite.data,
+            'patient_ID': form.patient_ID.data,
+            'optometriste_ID': form.optometriste_ID.data,
+            'histoireDeCas_ID': form.histoireDeCas_ID.data,
+        }
+        cursor.execute(f'SELECT RX_subjective FROM examens e WHERE patient_ID = {form.patient_ID.data} ORDER BY created_at DESC LIMIT 1;')
+        old_rx = cursor.fetchone()
+        if old_rx:
+            old_rx = parse_json_objects(old_rx) 
+            new_exam_data['old_RX'] = {}
+            for key, value in old_rx.items():
+                # Replace the key name
+                new_key = key.replace("RX_subjective_", "")
+                new_exam_data['old_RX'][new_key] = value
+        for key, value in new_exam_data.items():
+            if type(value) == dict:
+                try:
+                    new_exam_data[key] = json.dumps(value)
+                except (json.JSONDecodeError, TypeError):
+                    pass
+        if form.ID.data: #Existing exam
+            if old_rx:
+                sql_query = "UPDATE examens SET RX_objective = %s, RX_subjective = %s, contact_lens_type = %s, lens_type = %s, periode_validite = %s, patient_ID = %s, optometriste_ID = %s, histoireDeCas_ID = %s, old_RX = %s WHERE ID = %s"
+            else:
+                sql_query = "UPDATE examens SET RX_objective = %s, RX_subjective = %s, contact_lens_type = %s, lens_type = %s, periode_validite = %s, patient_ID = %s, optometriste_ID = %s, histoireDeCas_ID = %s WHERE ID = %s"
+            params = tuple(new_exam_data.values()) + (form.ID.data,)
+        else: #New exam
+            sql_query = "INSERT INTO examens (RX_objective, RX_subjective, contact_lens_type, lens_type, old_RX, periode_validite, patient_ID, optometriste_ID, histoireDeCas_ID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            params = tuple(new_exam_data.values())
+        cursor.execute(sql_query, params)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        session['confirmation_message'] = {"title": "Examen sauvegardée", "text": f"L'examen en cours a été sauvegardé avec succès."}
+        if form.ID.data: #Existing exam
+            return redirect(url_for("examens.details",examen_id=f"{session['examen']['ID']}", clinique_id=f"{session['clinique']['ID']}", patient_id=f"{session['patient']['ID']}"))
+        else: #New exam
+            return redirect(url_for("examens.new",examen_id=f"{session['examen']['ID']}", clinique_id=f"{session['clinique']['ID']}", patient_id=f"{session['patient']['ID']}"))
+        
+
+def parse_json_objects(dict):
     # Initialize a new dictionary to store extracted key-value pairs
     new_dict = {}
 
@@ -135,7 +305,7 @@ def parse_exam_json_objects(dict):
             for json_key, json_value in json_data.items():
                 new_key = f'{key}_{json_key}'  # Creating new keys by concatenating the original key with JSON keys
                 new_dict[new_key] = json_value
-        except (json.JSONDecodeError, TypeError):
+        except (json.JSONDecodeError, TypeError, AttributeError):
             # If parsing fails, simply copy the key-value pair to the new dictionary
             new_dict[key] = value
     
