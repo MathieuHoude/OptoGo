2c6f0d169e9f2246b50b1508c23b7c837485a8d6
Classican67 <117132942+Classican67@users.noreply.github.com>
2024-04-07 10:50:13 -0400

    retrait de la verification supp car bug

--- forms/exam_form.py
+++ forms/exam_form.py
@@ -8,9 +8,7 @@ def validate_rx_add(form, field):
         raise ValidationError('Value must be in steps of 0.25')
     
 
-def validate_RX(self, field):
-        if field.data is None:
-            field.data = '0'
+
 
 class ExamForm(FlaskForm):
     ID = HiddenField('ID')
@@ -18,22 +16,22 @@ class ExamForm(FlaskForm):
     optometriste_ID = HiddenField('ID optometriste')
     histoireDeCas_ID = HiddenField('ID histoire de cas')
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Sphere_LE = SelectField('Sphère', choices=choices, validators=[validate_rx_add, validate_RX])
-    old_RX_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add, validate_RX])
+    old_RX_Sphere_LE = SelectField('Sphère', choices=choices, validators=[validate_rx_add])
+    old_RX_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
     old_RX_Axis_LE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add, validate_RX])
+    old_RX_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
     old_RX_Acuity_LE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add, validate_RX])
-    old_RX_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add, validate_RX])
+    old_RX_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    old_RX_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
     old_RX_Axis_RE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    old_RX_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add, validate_RX])
+    old_RX_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
     old_RX_Acuity_RE = SelectField('Acuité', choices=choices)
 
@@ -47,42 +45,42 @@ class ExamForm(FlaskForm):
     contact_lens_type_mono_vision_contact_lenses = BooleanField('VC mono-vision', render_kw={"class":"w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"})
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[validate_rx_add, validate_RX])
-    RX_objective_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_objective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_objective_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_objective_Axis_LE = SelectField('Axe', choices=choices, validators=[validate_RX])
+    RX_objective_Axis_LE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_objective_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
     RX_objective_Acuity_LE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add, validate_RX])
-    RX_objective_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_objective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_objective_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_objective_Axis_RE = SelectField('Axe', choices=choices,validators=[validate_RX])
+    RX_objective_Axis_RE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_objective_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_objective_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
     RX_objective_Acuity_RE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[validate_rx_add, validate_RX])
-    RX_subjective_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_subjective_Sphere_LE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_subjective_Ast_LE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_subjective_Axis_LE = SelectField('Axe', choices=choices,validators=[validate_RX])
+    RX_subjective_Axis_LE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_subjective_Add_LE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
     RX_subjective_Acuity_LE = SelectField('Acuité', choices=choices)
 
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(-80, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add, validate_RX])
-    RX_subjective_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_subjective_Sphere_RE = SelectField('Sphere', choices=choices, validators=[validate_rx_add])
+    RX_subjective_Ast_RE = SelectField('Ast', choices=choices, validators=[validate_rx_add])
     choices = [(str(x), str(x)) for x in range(0, 181)]  # Generating options from 0 to 180
-    RX_subjective_Axis_RE = SelectField('Axe', choices=choices,validators=[validate_RX])
+    RX_subjective_Axis_RE = SelectField('Axe', choices=choices)
     choices = [("%.2f" % (x / 4.0), "%.2f" % (x / 4.0)) for x in range(0, 81)]  # Generating options from -20 to 20 by steps of 0.25
-    RX_subjective_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add, validate_RX])
+    RX_subjective_Add_RE = SelectField('Add', choices=choices, validators=[validate_rx_add])
     choices = [(1/6, '1/6'), (2/6, '2/6'), (3/6, '3/6'), (4/6, '4/6'), (5/6, '5/6'), (6/6, '6/6')]
     RX_subjective_Acuity_RE = SelectField('Acuité', choices=choices)
 
