8e7e2a2b5b6a3e3aaa1982425eacf3e5ab5dc8fb
MathieuHoude <mathieu.houde@codeboxx.biz>
2024-04-07 17:32:06 -0400

    address in patient form

--- forms/patient_form.py
+++ forms/patient_form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Email, Regexp
+from wtforms import StringField, DateField, SelectField
+from wtforms.validators import DataRequired, Email, Regexp, InputRequired
 
 class PatientForm(FlaskForm):
     first_name = StringField('Prénom', default='', validators=[DataRequired()])
     last_name = StringField('Nom', default='', validators=[DataRequired()])
     phone_number = StringField('Téléphone', default='', validators=[DataRequired(), Regexp(r'^\d{3}-\d{3}-\d{4}$', message="Format invalide, veuillez entrer un numéro sous la forme XXX-XXX-XXXX")])
-    gender = SelectField('Sexe', choices=[('Male', 'Homme'), ('Female', 'Femme'),('Genderqueer', 'Non-binaire'),('Bigender', 'Bigenre'),('Agender', 'Agenre'), ('Polygender', 'Polygenre'),('Other', 'Autre')])
+    gender = SelectField('Sexe', choices=[('Male', 'Homme'), ('Female', 'Femme'),('Genderqueer', 'Non-binaire'),('Bigender', 'Bigenre'),('Agender', 'Agenre'), ('Polygender', 'Polygenre'),('Other', 'Autre')], validators=[InputRequired(message="Veuillez choisir une option.")])
     email = StringField('Email', default='', validators=[DataRequired(), Email()])
     birth_date = DateField('Date de naissance', format='%Y-%m-%d', validators=[DataRequired()])
     RAMQ_number = StringField('RAMQ', default='', validators=[DataRequired(), Regexp(r'^[a-zA-Z]{4}\d{8}$', message="Format invalide. Devrait être sous la forme ABCD12345678")])
--- routes/patients_routes.py
+++ routes/patients_routes.py
@@ -2,7 +2,9 @@ from flask import render_template, request, redirect, url_for, session, Blueprin
 from mysql.connector import Error
 
 from forms.patient_form import PatientForm
+from forms.address_form import AddressForm
 from DB.utils import get_db_connection
+from utils import update_session
 
 patients_bp = Blueprint('patients', __name__, url_prefix='/patients')
 
@@ -22,10 +24,11 @@ def details(patient_id):
     index = 4
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute(f'SELECT * FROM patients WHERE ID = {patient_id}')
-    patient = cursor.fetchone()
-    session["patient"] = patient
-    form = PatientForm(data=patient)
+    update_session(cursor, "patient", f'SELECT * FROM patients WHERE ID = {patient_id}')
+    patient_form = PatientForm(data=session['patient'])
+    cursor.execute(f"SELECT * FROM addresses WHERE ID = {session['patient']['address_ID']}")
+    address = cursor.fetchone()
+    address_form = AddressForm(data=address)
     confirmation_message = session.pop('confirmation_message', None)
     cursor.close()
     conn.close()
@@ -35,7 +38,8 @@ def details(patient_id):
                            optometriste=session["user"],
                            patient=session["patient"],
                            confirmation_message=confirmation_message,
-                           form=form)
+                           patient_form=patient_form,
+                           address_form=address_form)
 
 # route pour la page de modification des informations du patient
 @patients_bp.route("/<int:patient_id>/edit", methods=['GET', 'POST'])
@@ -54,19 +58,17 @@ def edit(patient_id):
     If the request method is GET, the function retrieves the patient's information from the database, populates the form with the current information, and renders the edit page.
 
     """
-
     index = 4
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute(f'SELECT * FROM patients WHERE ID = {patient_id}')
-    patient = cursor.fetchone()
-    session["patient"] = patient
-    form = PatientForm(data=patient)
+    update_session(cursor, "patient", f'SELECT * FROM patients WHERE ID = {patient_id}')
+    address_form = AddressForm()
+    patient_form = PatientForm()
+    if request.method == 'POST' and patient_form.validate_on_submit():
 
-    if request.method == 'POST' and form.validate_on_submit():
         modified_fields = {}
-        for field in form:
-            if field.name != 'csrf_token' and field.data != patient[field.name]:
+        for field in patient_form:
+            if field.name != 'csrf_token' and field.data != session['patient'][field.name]:
                 modified_fields[field.name] = field.data
 
         if modified_fields:
@@ -79,11 +81,35 @@ def edit(patient_id):
 
             cursor.execute(sql_update_query, params)
             conn.commit()
-            cursor.close()
-            conn.close()
-            session['confirmation_message'] = {"title": "Modifications complétées", "text": f"Les informations de {patient['first_name']} {patient['last_name']} ont été mises à jour avec succès."}
-            return redirect(url_for("patients.details", patient_id=patient['ID']))
+
+        modified_fields = {}
+        cursor.execute(f"SELECT * FROM addresses WHERE ID = {session['patient']['address_ID']}")
+        address = cursor.fetchone()
+        for field in address_form:
+            if field.name != 'csrf_token' and field.data != address[field.name]:
+                modified_fields[field.name] = field.data
+
+        if modified_fields:
+            sql_update_query = "UPDATE addresses SET "
+            sql_update_query += ", ".join([f"{field} = %s" for field in modified_fields.keys()])
+            sql_update_query += " WHERE id = %s"
+            
+            # Create a tuple of parameter values in the same order as placeholders
+            params = tuple(modified_fields.values()) + (address['ID'],)
+
+            cursor.execute(sql_update_query, params)
+            conn.commit()
+
+        cursor.close()
+        conn.close()
+        session['confirmation_message'] = {"title": "Modifications complétées", "text": f"Les informations de {session['patient']['first_name']} {session['patient']['last_name']} ont été mises à jour avec succès."}
+        return redirect(url_for("patients.details", patient_id=session['patient']['ID']))
     else:
+        update_session(cursor, "patient", f'SELECT * FROM patients WHERE ID = {patient_id}')
+        patient_form = PatientForm(data=session['patient'])
+        cursor.execute(f"SELECT * FROM addresses WHERE ID = {session['patient']['address_ID']}")
+        address = cursor.fetchone()
+        address_form = AddressForm(data=address)
         cursor.close()
         conn.close()
         return render_template("patientEditPage.html",
@@ -91,7 +117,8 @@ def edit(patient_id):
                             clinique=session["clinique"],
                             optometriste=session["user"],
                             patient=session["patient"],
-                            form=form)
+                            patient_form=patient_form,
+                            address_form=address_form)
     
 
 # route pour la page de création d'un nouveau patient
@@ -109,23 +136,40 @@ def new():
     """
     index=2.5
     clinique_id = request.args.get('clinique_id')
-    form = PatientForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    patient_form = PatientForm()
+    address_form = AddressForm()
+    if request.method == 'POST' and patient_form.validate_on_submit() and address_form.validate_on_submit():
         try :
-            new_patient_data = {
-                'first_name': form.first_name.data,
-                'last_name': form.last_name.data,
-                'birth_date': form.birth_date.data,
-                'gender': form.gender.data,
-                'email': form.email.data,
-                'phone_number': form.phone_number.data,
-                'RAMQ_number': form.RAMQ_number.data,
+            new_address_data = {
+                'street_number': address_form.street_number.data,
+                'street_name': address_form.street_name.data,
+                'city': address_form.city.data,
+                'province': address_form.province.data,
+                'postal_code': address_form.postal_code.data,
             }
 
             conn = get_db_connection()
             cursor = conn.cursor(dictionary=True)
 
-            sql_insert_patient_query = "INSERT INTO patients (first_name, last_name, birth_date, gender, email, phone_number, RAMQ_number) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            sql_insert_address_query = "INSERT INTO addresses (street_number, street_name, city, province, postal_code) VALUES (%s, %s, %s, %s, %s)"
+            params = tuple(new_address_data.values())
+            cursor.execute(sql_insert_address_query, params)
+            conn.commit()
+
+            new_address_id = cursor.lastrowid
+
+            new_patient_data = {
+                'first_name': patient_form.first_name.data,
+                'last_name': patient_form.last_name.data,
+                'birth_date': patient_form.birth_date.data,
+                'gender': patient_form.gender.data,
+                'email': patient_form.email.data,
+                'phone_number': patient_form.phone_number.data,
+                'RAMQ_number': patient_form.RAMQ_number.data,
+                'address_ID': new_address_id
+            }
+
+            sql_insert_patient_query = "INSERT INTO patients (first_name, last_name, birth_date, gender, email, phone_number, RAMQ_number, address_ID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
             params = tuple(new_patient_data.values())
             cursor.execute(sql_insert_patient_query, params)
             conn.commit()
@@ -147,7 +191,8 @@ def new():
                 clinique=session["clinique"],
                 optometriste=session["user"],
                 error_message={'title': 'Erreur lors de la création du nouveau patient', 'message': e.msg, 'code': e.errno},
-                form=form)
+                patient_form=patient_form,
+                address_form=address_form)
 
     try:
         conn = get_db_connection()
@@ -161,6 +206,7 @@ def new():
             index=index,
             clinique=session["clinique"],
             optometriste=session["user"],
-            form=form)
+            patient_form=patient_form,
+            address_form=address_form)
     except Error as e:
         print(f"Error with MySQL: {e}")
